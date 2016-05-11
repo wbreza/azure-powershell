db517efd0e228fe2afcbb517bd43ef91c8bbf4ad
Wallace Breza <wabreza@microsoft.com>
2016-05-11 16:07:52 -0700

    Initial commit for pbie powershell

---
+++ src/ResourceManager/PowerBIEmbedded/Commands.Management.PowerBIEmbedded/WorkspaceCollection/GetWorkspaceCollection.cs
@@ -0,0 +1,77 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.Management.PowerBIEmbedded.Models;
+using Microsoft.Azure.Management.PowerBIEmbedded;
+
+namespace Microsoft.Azure.Commands.Management.PowerBIEmbedded.WorkspaceCollection
+{
+    [Cmdlet(VerbsCommon.Get, WorkspaceCollectionNounStr), OutputType(typeof(PSWorkspaceCollection))]
+    public class GetWorkspaceCollection : WorkspaceCollectionBaseCmdlet
+    {
+        protected const string ResourceGroupParameterSet = "ResourceGroupParameterSet";
+        protected const string WorkspaceCollectionNameParameterSet = "WorkspaceCollectionNameParameterSet";
+
+        [Parameter(
+            Position = 0,
+            Mandatory = false,
+            ParameterSetName = ResourceGroupParameterSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Resource Group Name.")]
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = WorkspaceCollectionNameParameterSet,
+            HelpMessage = "Workspace Collection Name.")]
+
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        public async override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            var workspaceCollections = new List<Azure.Management.PowerBIEmbedded.Models.WorkspaceCollection>();
+
+            // Workspace collections within a subscription
+            if (string.IsNullOrWhiteSpace(this.Name) && string.IsNullOrWhiteSpace(this.ResourceGroupName))
+            {
+                var subscriptionCollections = await this.PowerBIClient.GetWorkspacesCollectionsBySubscriptionAsync(this.SubscriptionId, ArmApiVersion);
+                workspaceCollections.AddRange(subscriptionCollections.Value);
+            }
+
+            // Workspace collections within a resource group
+            if (string.IsNullOrWhiteSpace(this.Name) && !string.IsNullOrWhiteSpace(this.ResourceGroupName))
+            {
+                var resourceGroupCollections = await this.PowerBIClient.GetWorkspacesCollectionsByResourceGroupAsync(this.SubscriptionId, this.ResourceGroupName, ArmApiVersion);
+                workspaceCollections.AddRange(resourceGroupCollections.Value);
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.Name) && !string.IsNullOrWhiteSpace(this.ResourceGroupName))
+            {
+                var workspace = await this.PowerBIClient.GetWorkspaceCollectionAsync(this.SubscriptionId, this.ResourceGroupName, this.Name, ArmApiVersion);
+                workspaceCollections.Add(workspace);
+            }
+
+            this.WriteWorkspaceCollectionList(workspaceCollections);
+        }
+    }
+}
---
+++ src/ResourceManager/PowerBIEmbedded/Commands.Management.PowerBIEmbedded/WorkspaceCollection/WorkspaceCollectionBaseCmdlet.cs
@@ -0,0 +1,62 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.Azure.Commands.Common.Authentication.Models;
+using Microsoft.Azure.Commands.Management.PowerBIEmbedded.Models;
+using Microsoft.Azure.Commands.ResourceManager.Common;
+using Microsoft.Azure.Common.Authentication;
+using Microsoft.Azure.Management.PowerBIEmbedded;
+using Microsoft.WindowsAzure.Commands.Utilities.Common;
+
+namespace Microsoft.Azure.Commands.Management.PowerBIEmbedded.WorkspaceCollection
+{
+    public class WorkspaceCollectionBaseCmdlet : AzureRMCmdlet
+    {
+        protected const string WorkspaceCollectionNounStr = "AzureRmPowerBIWorkspaceCollection";
+        protected const string ArmApiVersion = "2016-01-29";
+
+        private IPowerBIEmbeddedManagementClient powerBIClient;
+
+        protected IPowerBIEmbeddedManagementClient PowerBIClient
+        {
+            get
+            {
+                if (this.powerBIClient == null)
+                {
+                    this.powerBIClient = AzureSession.ClientFactory.CreateArmClient<PowerBIEmbeddedManagementClient>(DefaultProfile.Context, AzureEnvironment.Endpoint.ResourceManager);
+                    return this.powerBIClient;
+                }
+                else
+                {
+                    return this.powerBIClient;
+                }
+            }
+        }
+
+        public string SubscriptionId => DefaultProfile.Context.Subscription.Id.ToString();
+
+        protected void WriteWorkspaceCollection(Azure.Management.PowerBIEmbedded.Models.WorkspaceCollection workspaceCollection)
+        {
+            WriteObject(PSWorkspaceCollection.Create(workspaceCollection));
+        }
+
+        protected void WriteWorkspaceCollectionList(IEnumerable<Azure.Management.PowerBIEmbedded.Models.WorkspaceCollection> workspaceCollections)
+        {
+            List<PSWorkspaceCollection> output = new List<PSWorkspaceCollection>();
+            workspaceCollections.ForEach(workspaceCollection => output.Add(PSWorkspaceCollection.Create(workspaceCollection)));
+            WriteObject(output, true);
+        }
+    }
+}
